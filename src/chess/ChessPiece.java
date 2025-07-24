7ce36f5310fa875004f451e5b904af054dcb36d8
Renanborgess <renanborgessti@gmail.com>
2025-07-24 20:29:25 -0300

    Pawn

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -5,7 +5,7 @@ import boardgame.Piece;
 import boardgame.Position;
 
 public abstract class ChessPiece extends Piece {
-	
+
 	private Color color;
 	private int moveCount;
 
@@ -18,21 +18,24 @@ public abstract class ChessPiece extends Piece {
 		return color;
 	}
 	
-	public void increaseMoveCount() {
+	public int getMoveCount() {
+		return moveCount;
+	}
+	
+	protected void increaseMoveCount() {
 		moveCount++;
 	}
-	
-	public void decreaseMoveCount() {
+
+	protected void decreaseMoveCount() {
 		moveCount--;
 	}
-	
-	public ChessPosition getchessPosition() {
+
+	public ChessPosition getChessPosition() {
 		return ChessPosition.fromPosition(position);
 	}
 	
 	protected boolean isThereOpponentPiece(Position position) {
 		ChessPiece p = (ChessPiece)getBoard().piece(position);
 		return p != null && p.getColor() != color;
-		
 	}
 }
